7596a4c96be3c5ae6f8210bd2fa0fe66ec5ad0c2
mgdelmonte <mgdelmonte@gmail.com>
2021-09-30 20:52:46 -0400

    more

--- eoas.py
+++ eoas.py
@@ -1,21 +1,31 @@
 import datetime
+import os
 import re
 import requests
 import lxml.html
 import urllib
+from dateutil.parser import parse
 
+
+def date_of(s):
+    try: return parse(s)
+    except: return None
 
 
 def add_zczc(text):
     return re.sub(r"(\n\n|\A\n*)(\d+)", r"\1ZCZC \2", text)
 
 
-def fetch(proxy=None):
+def fetch(date=None, proxy=None):
     """Fetches all EOAS files from the syn folder, prefixes ZCZCnnn to each message,
-    and writes the combined files as YYYY-MM-DDgts.txt
+    and writes the combined files as YYYY-MM-DDgts.txt.
+    :param date: the date to get; defaults to "today"
     :param proxy: optional host:port to use as proxy
     """
-    fn = datetime.date.today().strftime("%Y-%m-%d-EOAS-GTS.txt")
+    date = date_of(date) or datetime.date.today()
+    fn = date.strftime("%Y-%m-%d-EOAS-GTS.txt")
+    if os.path.exists(fn):
+        os.remove(fn)
     session = requests.Session()
     if proxy:
         if not re.match("https?://", proxy):
@@ -23,6 +33,7 @@ def fetch(proxy=None):
         session.proxies = dict(http=proxy, https=proxy)
     # todo is a list of all the URLs we want to fetch
     todo = ["http://rawdata.eoas.fsu.edu/syn/"]
+    wanted = re.compile(f"{date.strftime('%Y%m%d')}/$|\.syn$")
     while todo:
         url = todo.pop()
         print(url)
@@ -46,8 +57,8 @@ def fetch(proxy=None):
             # if u isn't longer than where we started, then it's going "up" the tree, so ignore it
             if len(u) <= len(url):
                 continue
-            # if u ends with a slash or a .syn extension, put it on the todo list
-            if u.endswith("/") or u.endswith(".syn"):
+            # if u is wanted, put it on the todo list
+            if wanted.search(u):
                 todo.append(u)
 
 
